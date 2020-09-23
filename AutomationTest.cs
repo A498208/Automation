105dd33cec6c1020da91c643b9dcff569c84dddd
A498208 <282776167@qq.com>
2020-09-23 11:14:30 +0800

    Add files via upload
    init check in

---
+++ AutomationTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ioh.Automation.Test
+{
+    [TestClass]
+    public class AutomationTest
+    {
+        [TestMethod]
+        public void CallBai_Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
